5bcc18c1fc2a4bc181dfda73d7b564a9df9a61fe
Matteo Burbui <mattealex@gmail.com>
2016-09-24 17:27:08 +0200

    Revert "Ottimizzazione stile about-home"
    This reverts commit 9617987df2224f698f8932f7ec5898c9c1ecd310.

--- include/view/frame/template/main/intro/en.php
+++ include/view/frame/template/main/intro/en.php
@@ -9,9 +9,6 @@
                                                                                     In high school I studied pedagogy, but it wasn't my way, so during the school time I studied HTML, PHP and JavaScript.<br>
                                                                                     I worked as a typesetter and here I made some little programs for the company like custom calculators, work diary or utility.
 										</p>
-                                                                                <ul class="actions">
-											<li><a href="<?php echo Pageloader::pageFromTarget("about-me", false,"en");  ?>" class="button">Read More</a></li>
-										</ul>
 									</div>
 									<span class="image"><img src="/imgs/me.jpg" alt="Matteo Burbui" title="Matteo Burbui" /></span>
 								</div>
--- include/view/frame/template/nav/it.php
+++ include/view/frame/template/nav/it.php
@@ -1,6 +1,8 @@
 					<nav id="nav">
 						<ul>
-							<li><a class="nav_section" href="<?php echo Pageloader::pageFromTarget("home", false,"it");  ?>">Home</a></li>
+							<li><a class="nav_section" href="#presentazione">Presentazione</a></li>
+							<li><a class="nav_section" href="#competenze">Competenze</a></li>
+							<li><a class="nav_section" href="#lavorisvolti">Lavori Svolti</a></li>
 							<li><a href="<?php echo Pageloader::pageFromTarget(Pageloader::getData("target"), false,"en");  ?>"><img style="width: 24px;" src="/imgs/flagEN.svg" alt="In Enlish"></a></li>
 						</ul>
 					</nav>
