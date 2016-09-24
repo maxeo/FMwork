82c131249f6d26bcab777c265dec8f7268aa6300
Matteo Burbui <mattealex@gmail.com>
2016-09-24 18:11:01 +0200

    Restrore Poroject
    Project is restored

--- include/view/frame/exemple2/en.php
+++
@@ -1,2 +0,0 @@
-It's <?php echo date("G:i",time()) ?>.
-<a href="/it">Change language to italian</a>
--- include/view/frame/exemple2/it.php
+++
@@ -1,2 +0,0 @@
-Sono le <?php echo date("G:i",time()) ?>.
-<a href="/en">Cambia la lingua in inglese</a>
