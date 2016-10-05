2b0059bc23f41d4981cebb2c7004bce6ac2d671d
Matteo Burbui <mattealex@gmail.com>
2016-10-05 16:50:20 +0200

    Auto redirect home

--- include/view/bone/home/index.php
+++ include/view/bone/home/index.php
@@ -1,30 +1,34 @@
 <?php
-Pageloader::addTag("!DOCTYPE html", NULL, 0, 1);
-Pageloader::ln();
-Pageloader::addTag('html lang="'.Pageloader::getData("lang").'"', NULL, 0, 1);
-Pageloader::ln();
-Pageloader::addElement("tHead");
-Pageloader::ln();
-Pageloader::addTag("body", NULL, 0, 1);
-Pageloader::ln();
-Pageloader::addTag(' div id="wrapper"', NULL, 0, 1);
-Pageloader::ln();
-Pageloader::addElement("tHeaderHome");
-Pageloader::ln();
-Pageloader::addElement("tNavHome");
-Pageloader::ln();
-Pageloader::addTag(' div id="main"', NULL, 0, 1);
-Pageloader::ln();
-Pageloader::addElement("tAbout-me");
-Pageloader::ln();
-Pageloader::addElement("tMainSkills");
-Pageloader::ln();
-Pageloader::addElement("tMainWorksdone");
-Pageloader::ln();
-Pageloader::addTag(' div', NULL, 1, 0);
-Pageloader::ln();
-Pageloader::addElement("tFooter");
-Pageloader::ln();
-Pageloader::addTag(' div', NULL, 1, 0);
 
+if (@explode("/", $_SERVER['REQUEST_URI'])[1] != Pageloader::getData("lang")) {
+    header("Location: " . Pageloader::getData("lang") . "/");
+} else {
+    Pageloader::addTag("!DOCTYPE html", NULL, 0, 1);
+    Pageloader::ln();
+    Pageloader::addTag('html lang="' . Pageloader::getData("lang") . '"', NULL, 0, 1);
+    Pageloader::ln();
+    Pageloader::addElement("tHead");
+    Pageloader::ln();
+    Pageloader::addTag("body", NULL, 0, 1);
+    Pageloader::ln();
+    Pageloader::addTag(' div id="wrapper"', NULL, 0, 1);
+    Pageloader::ln();
+    Pageloader::addElement("tHeaderHome");
+    Pageloader::ln();
+    Pageloader::addElement("tNavHome");
+    Pageloader::ln();
+    Pageloader::addTag(' div id="main"', NULL, 0, 1);
+    Pageloader::ln();
+    Pageloader::addElement("tAbout-me");
+    Pageloader::ln();
+    Pageloader::addElement("tMainSkills");
+    Pageloader::ln();
+    Pageloader::addElement("tMainWorksdone");
+    Pageloader::ln();
+    Pageloader::addTag(' div', NULL, 1, 0);
+    Pageloader::ln();
+    Pageloader::addElement("tFooter");
+    Pageloader::ln();
+    Pageloader::addTag(' div', NULL, 1, 0);
+}
 ?>
