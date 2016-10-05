bc781b28dcee1c08a4af30b7099595a0e1e2e952
Matteo Burbui <mattealex@gmail.com>
2016-10-05 21:31:11 +0200

    x-defaul

--- include/view/skin/head.php
+++ include/view/skin/head.php
@@ -1,22 +1,22 @@
     <head>
-        <title><?php $thisPgTarget = Pageloader::getData("target"); if($thisPgTarget=="home")echo "Matteo Burbui - "; ?>Maxeo.it<?php if (!empty($thisPgTarget) && $thisPgTarget != "home")echo " - " . ucwords(str_replace("-", " ", Pageloader::getData("page"))); ?></title>
+        <title><?php $thisPgTarget = Pageloader::getData("target");if ($thisPgTarget == "home")echo "Matteo Burbui - "; ?>Maxeo.it<?php if (!empty($thisPgTarget) && $thisPgTarget != "home") echo " - " . ucwords(str_replace("-", " ", Pageloader::getData("page"))); ?></title>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="/style/main.css" />
         <link rel="stylesheet" href="/style/max.css" />
         <?php
-            if (Pageloader::getData("lang") == "en")
-                $seo_alternate_lang = "it";
-            else
-                $seo_alternate_lang = "en";
-            $seo_alternate_href = Pageloader::pageFromTarget(Pageloader::getData("target"), false, $seo_alternate_lang);
-            if (!empty(Pageloader::getData(0)))
-                $seo_alternate_href.="/" . Pageloader::getData(0);
-            
-            if (@explode("/", $_SERVER['REQUEST_URI'])[1] != Pageloader::getData("lang")){
-                $seo_alternate_href="/";
-                $seo_alternate_lang= "x-default";
-            }
+        if (Pageloader::getData("lang") == "en")
+            $seo_alternate_lang = "it";
+        else
+            $seo_alternate_lang = "en";
+        $seo_alternate_href = Pageloader::pageFromTarget(Pageloader::getData("target"), false, $seo_alternate_lang);
+        for ($i = 0; !empty(Pageloader::getData($i)); $i++) 
+            $seo_alternate_href.="/" . Pageloader::getData(0);
+
+        if (@explode("/", $_SERVER['REQUEST_URI'])[1] != Pageloader::getData("lang")) {
+            $seo_alternate_href = "/";
+            $seo_alternate_lang = "x-default";
+        }
         ?>
         <link rel="alternate" href="<?php echo $seo_alternate_href; ?>" hreflang="<?php echo $seo_alternate_lang; ?>" type=text/html>
         <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
