3dfeb4482b2bcd2df04d98283e8ffeaa9a2b527b
Matteo Burbui <mattealex@gmail.com>
2016-10-06 10:46:26 +0200

    errore di battitura

--- include/view/skin/subsection/about-me/it.php
+++ include/view/skin/subsection/about-me/it.php
@@ -6,7 +6,7 @@
                     </header>
                     <p>
                         <span class="image maxPhoto"><img src="/imgs/me.jpg" alt="Matteo Burbui" title="Matteo Burbui" /></span>
-                        Mi chiamo Mattteo Burbui, sono nato a Figline Valdarno il 17 Agosto 1990. Al momento vivo a San Giovanni Valdarno.<br>
+                        Mi chiamo Matteo Burbui, sono nato a Figline Valdarno il 17 Agosto 1990. Al momento vivo a San Giovanni Valdarno.<br>
                         Ho frequentato il liceo socio-psico pedagogico, ma ho capito che non era la strada che faceva per me, così, durante il periodo scolastico, ho studiato <b>HTML</b>, <b>PHP</b> e <b>JavaScript</b>.<br>
                         Ho lavorato come tipografo per l'etichettificio <a href="http://www.ogasrl.it/"><b>Oga</b></a> per tre anni, e qui ho fatto alcuni piccoli programmi per l'azienda come calcolatrici personalizzati, un diario di lavoro o altri programmi di utilità.<br>
                         Nonostante avessi un buono stipendio e tutto sommato non me la passavo male, dopo tre anni, ho deciso di tornare a studiare, iniziai iscrivendomi ad ingegneria informatica.
