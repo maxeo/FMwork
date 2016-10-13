e09a035c91c9cc71a0b939cb80c4384fccae5f37
Matteo Burbui <mattealex@gmail.com>
2016-10-13 18:54:22 +0200

    correzioni del testo

--- include/view/skin/nav_home/en.php
+++ include/view/skin/nav_home/en.php
@@ -1,6 +1,6 @@
 					<nav id="nav">
 						<ul>
-							<li><a class="nav_section"  href="#introduction" class="active">Introduction</a></li>
+							<li><a class="nav_section"  href="#introduction" class="active">About Me</a></li>
 							<li><a class="nav_section"  href="#skills">Skills</a></li>
 							<li><a class="nav_section"  href="#worksdone">Works Done</a></li>
 							<li><a href="<?php echo Pageloader::pageFromTarget(Pageloader::getData("target"), false,"it");if(!empty(Pageloader::getData(0)))echo "/".Pageloader::getData(0); ?>"><img style="width: 24px;"><img style="width: 24px;" src="/imgs/flagIT.svg" alt="In Italiano"></a></li>
