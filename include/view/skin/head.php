9f1b6c805653ff90c467d856350f7963e4b9edd8
Matteo Burbui <mattealex@gmail.com>
2016-10-13 19:50:21 +0200

    added icon

--- include/view/skin/head.php
+++ include/view/skin/head.php
@@ -1,7 +1,27 @@
 <head>
-    <title><?php $thisPgTarget = Pageloader::getData("target");echo "Matteo Burbui - "; ?>Maxeo.it<?php if (!empty($thisPgTarget) && $thisPgTarget != "home") echo " - " . ucwords(str_replace("-", " ", Pageloader::getData("page"))); ?></title>
+    <title><?php $thisPgTarget = Pageloader::getData("target");
+echo "Matteo Burbui - "; ?>Maxeo.it<?php if (!empty($thisPgTarget) && $thisPgTarget != "home") echo " - " . ucwords(str_replace("-", " ", Pageloader::getData("page"))); ?></title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+    <link rel="apple-touch-icon" sizes="57x57" href="/imgs/icon/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/imgs/icon/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/imgs/icon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/imgs/icon/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/imgs/icon/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/imgs/icon/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/imgs/icon/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/imgs/icon/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/imgs/icon/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="/imgs/icon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/imgs/icon/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="/imgs/icon/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/imgs/icon/manifest.json">
+    <link rel="mask-icon" href="/imgs/icon/safari-pinned-tab.svg" color="#d55b5b">
+    <meta name="msapplication-TileColor" content="#603cba">
+    <meta name="msapplication-TileImage" content="/imgs/icon/mstile-144x144.png">
+    <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="/style/main.css" />
     <link rel="stylesheet" href="/style/max.css" />
     <?php
