30befcb697a7449cb212f1105fb831a7a1d8b7ca
Matteo Burbui <info@maxeo.it>
2016-11-23 16:53:57 +0100

    clean

--- _include/view/skin/message/en.php
+++ _include/view/skin/message/en.php
@@ -1,21 +1,19 @@
-							<section id="presentazione" class="main">
-								<div class="spotlight">
-									<div class="content">
-                                                                            <?php
-                                                                            $txt="";
-                                                                            if(Pageloader::getData(0)!==false){	//c'è un messaggio da comunicare
-                                                                                    $m_code=Pageloader::getData(0);
-                                                                                    $mess=Notification::showCode($m_code,NULL,true);
-                                                                                    if($mess){
-                                                                                            $txt=$mess['description'];
-                                                                                    }
-                                                                                    else
-                                                                                            $txt="<p>Unknown Message</p>";
+<section id="presentazione" class="main">
+    <div class="spotlight">
+        <div class="content">
+            <?php
+            $txt = "";
+            if (Pageloader::getData(0) !== false) { //c'è un messaggio da comunicare
+                $m_code = Pageloader::getData(0);
+                $mess = Notification::showCode($m_code, NULL, true);
+                if ($mess) {
+                    $txt = $mess['description'];
+                } else
+                    $txt = "<p>Unknown Message</p>";
+            }
+            echo $txt;
+            ?>
+        </div>
 
-                                                                            }
-                                                                            echo $txt;
-                                                                            ?>
-									</div>
-									
-								</div>
-							</section>
+    </div>
+</section>
